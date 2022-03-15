67facedcc92fd67ba73ea52ecb27b831ef07c381
yamank41 <100734289+yamank41@users.noreply.github.com>
2022-03-15 21:02:23 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+var Name = "yaman"
+var birthdayYear = "2005"
+var age = 2022 - Int(birthdayYear)!
+
+print ("my name is \(Name) and my age is \(age)")
+
+var k = "8"
+var l = 4
+var j = 2.5
+var s = (Int(k) ?? 0) + l
+
+
 // التمرين الأول
 // ClassWork 1
 
